8004bd93daa56cd9dd45d613a19f08dc53cb240a
unknown <thandiwenjephe@gmail.com>
2020-10-16 13:11:46 +0200

    Add IT_Management and Asset_Management form

--- ProjectManagementToolkit/MainForm.cs
+++ ProjectManagementToolkit/MainForm.cs
@@ -16,5 +16,22 @@ namespace ProjectManagementToolkit
         {
             InitializeComponent();
         }
+
+        private void btnItManagement_Click(object sender, EventArgs e)
+        {
+            tabControl2.Visible = false;
+            tabControl1.Visible = true;
+        }
+
+        private void btnAssetsAndInventory_Click(object sender, EventArgs e)
+        {
+            tabControl2.Visible = true;
+            tabControl1.Visible = false;
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
--- ProjectManagementToolkit/Program.cs
+++ ProjectManagementToolkit/Program.cs
@@ -16,7 +16,7 @@ namespace ProjectManagementToolkit
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Login());
+            Application.Run(new MainForm());
         }
     }
 }
