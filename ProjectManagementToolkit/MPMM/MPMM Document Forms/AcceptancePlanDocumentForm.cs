25c5ef210922331a8a5bce7b2b70e859ad9c19f5
Fraser <28635248@student.g.nwu.ac.za>
2020-10-23 19:11:04 +0200

    Acceptance Plan Front End
    Acceptance Plan Front end completed, back end needed to be implemented

--- ProjectManagementToolkit/MPMM/MPMM Document Forms/AcceptancePlanDocumentForm.cs
+++ ProjectManagementToolkit/MPMM/MPMM Document Forms/AcceptancePlanDocumentForm.cs
@@ -16,5 +16,10 @@ namespace ProjectManagementToolkit.MPMM.MPMM_Document_Forms
         {
             InitializeComponent();
         }
+
+        private void AcceptancePlanDocumentForm_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
