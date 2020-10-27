8f9f7c3f98843b46e739f90ca545cb6c85e852ae
Wessel <wessel7.lourens@gmail.com>
2020-10-27 15:33:31 +0200

    Term of Reference Document
    Completed front-end

--- ProjectManagementToolkit/MPMM/MPMM Document Forms/TermOfReferenceDocumentForm.cs
+++ ProjectManagementToolkit/MPMM/MPMM Document Forms/TermOfReferenceDocumentForm.cs
@@ -16,5 +16,30 @@ namespace ProjectManagementToolkit.MPMM.MPMM_Document_Forms
         {
             InitializeComponent();
         }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void vScrollBar1_Scroll(object sender, ScrollEventArgs e)
+        {
+
+        }
+
+        private void tabAppendix_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
