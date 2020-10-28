976d954cb96f2f078be1c3dc068e0ad2c723fe65
Bongiwe Sibanda <72738832+bongiine@users.noreply.github.com>
2020-10-28 21:27:09 +0200

    Completed

--- ProjectManagementToolkit/MPMM/MPMM Document Forms/StatementOfWorkDocumentForm.cs
+++ ProjectManagementToolkit/MPMM/MPMM Document Forms/StatementOfWorkDocumentForm.cs
@@ -16,5 +16,30 @@ namespace ProjectManagementToolkit.MPMM.MPMM_Document_Forms
         {
             InitializeComponent();
         }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView3_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
