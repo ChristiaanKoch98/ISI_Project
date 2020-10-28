74908d8619809d35696d9d133552698f4ba836db
Mpho-Joy <72738235+Mpho-Joy@users.noreply.github.com>
2020-10-28 21:43:11 +0200

    Complete Change Request Form

--- ProjectManagementToolkit/MPMM/MPMM Document Forms/ChangeRequestFormDocumentForm.cs
+++ ProjectManagementToolkit/MPMM/MPMM Document Forms/ChangeRequestFormDocumentForm.cs
@@ -16,5 +16,10 @@ namespace ProjectManagementToolkit.MPMM.MPMM_Document_Forms
         {
             InitializeComponent();
         }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
     }
 }
