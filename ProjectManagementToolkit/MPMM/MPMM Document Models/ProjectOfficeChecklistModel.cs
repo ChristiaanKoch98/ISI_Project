6b5bb27c225b3ccce15e44b2af0b30af0068319b
Gerard Jooste <28410688@student.g.nwu.ac.za>
2020-10-30 17:16:45 +0200

    Ok Now I'm finished with everything. That OfficeCheckList is WAY to big

--- ProjectManagementToolkit/MPMM/MPMM Document Models/ProjectOfficeChecklistModel.cs
+++ ProjectManagementToolkit/MPMM/MPMM Document Models/ProjectOfficeChecklistModel.cs
@@ -14,6 +14,8 @@ namespace ProjectManagementToolkit.MPMM.MPMM_Document_Models
 
         public string ProjectOfficeManager { get; set; }
 
+        /*Premesis*/
+
         public Boolean PremisesProjectOfficeDocumented { get; set; }
 
         public Boolean PremisesProjectOfficeProcured { get; set; }
@@ -32,6 +34,8 @@ namespace ProjectManagementToolkit.MPMM.MPMM_Document_Models
 
         public Boolean PremisesOnSiteFacilities { get; set; }
 
+        /*Equipement*/
+
         public Boolean EquipmentRequiredOffice { get; set; }
 
         public Boolean EquipmentMaintananceCOntracts { get; set; }
@@ -46,6 +50,8 @@ namespace ProjectManagementToolkit.MPMM.MPMM_Document_Models
 
         public Boolean EquipmentFunctioningAsRequired { get; set; }
 
+        /*Roles*/
+
         public Boolean RolesAppointedProjectSponsor { get; set; }
         public Boolean RolesAppointedProjectManager { get; set; }
         public Boolean RolesAppointedProjectOfficeManager { get; set; }
@@ -58,12 +64,15 @@ namespace ProjectManagementToolkit.MPMM.MPMM_Document_Models
         public Boolean RolesJobDescriptionsResponsibilities { get; set; }
         public Boolean RolesSkilledPeopleAppointed { get; set; }
 
-        public Boolean StandardsIndustyStandards { get; set; }
+        /*Standards and Processes*/
 
+        //Standards
+        public Boolean StandardsIndustyStandards { get; set; }
         public Boolean StandardsHealthAndSafety { get; set; }
         public Boolean StandardsProjectPlanning { get; set; }
         public Boolean StandardsPMI { get; set; }
 
+        //Processes
         public Boolean ProcessesTimeManagement { get; set; }
         public Boolean ProcessesCostManagement { get; set; }
         public Boolean ProcessesQualityManagement { get; set; }
@@ -74,18 +83,28 @@ namespace ProjectManagementToolkit.MPMM.MPMM_Document_Models
         public Boolean ProcessesAcceptanceManagement { get; set; }
         public Boolean ProcessesCommunicationsManagement { get; set; }
 
+        /*Templates*/
+
+        //Initiation
         public Boolean TemplatesInitiationBusinessCase { get; set; }
         public Boolean TemplatesInitiationFeasibilityStudy { get; set; }
         public Boolean TemplatesInitiationTermsOfReference { get; set; }
         public Boolean TemplatesInitiationJobDescription { get; set; }
         public Boolean TemplatesInitiationStageGate { get; set; }
 
+        //Planning
         public Boolean TemplatesPlanningProjectPlan { get; set; }
         public Boolean TemplatesPlanningResourcePlan { get; set; }
         public Boolean TemplatesPlanningFinancialPlan { get; set; }
         public Boolean TemplatesPlanningQualityPlan { get; set; }
         public Boolean TemplatesPlanningRiskPlan { get; set; }
+        public Boolean TemplatesPlanningAcceptancePlan { get; set; }
+        public Boolean TemplatesPlanningCommunicationsPlan { get; set; }
+        public Boolean TemplatesPlanningProcurementPlan { get; set; }
+        public Boolean TemplatesPlanningSupplierPlan { get; set; }
+        public Boolean TemplatesPlanningTenderPlan { get; set; }
 
+        //Execution
         public Boolean TemplatesExecutionTimesheet { get; set; }
         public Boolean TemplatesExecutionExpense { get; set; }
         public Boolean TemplatesExecutionQuality { get; set; }
@@ -98,8 +117,90 @@ namespace ProjectManagementToolkit.MPMM.MPMM_Document_Models
         public Boolean TemplatesExecutionCommunication { get; set; }
         public Boolean TemplatesExecutionAcceptance { get; set; }
 
+        //Closure
+        public Boolean TemplatesClosureProjectReport { get; set; }
+        public Boolean TemplatesClosurePostReview { get; set; }
+
+        /*Services*/
+
+        //TimeManagement
+        public Boolean ServicesTimeMonitoring { get; set; }
+        public Boolean ServicesTimeProjectPlan { get; set; }
+        public Boolean ServicesTimeTimesheet { get; set; }
+
+        //Cost Management
+        public Boolean ServicesCostMonitoring { get; set; }
+        public Boolean ServicesCostProjectPlan { get; set; }
+        public Boolean ServicesCostExpense { get; set; }
+
+        //Quality Management
+        public Boolean ServicesQualityAssurance { get; set; }
+        public Boolean ServicesQualityControl { get; set; }
+        public Boolean ServicesQualityDeliverables { get; set; }
+
+        //Change Management
+        public Boolean ServicesChangeRequests { get; set; }
+        public Boolean ServicesChangeSheduling { get; set; }
+        public Boolean ServicesChangeKeeping { get; set; }
+
+        //Risk Management
+        public Boolean ServicesRiskForms { get; set; }
+        public Boolean ServicesRiskSheduling { get; set; }
+        public Boolean ServicesRiskKeeping { get; set; }
+
+        //Issue Management
+        public Boolean ServicesIssueForms { get; set; }
+        public Boolean ServicesIssueScheduling { get; set; }
+        public Boolean ServicesIssueKeeping { get; set; }
 
 
+        //Procurement Management
+        public Boolean ServicesProcurementPurchase { get; set; }
+        public Boolean ServicesProcurementGoodsAndServices { get; set; }
+        public Boolean ServicesProcurementKeeping { get; set; }
+        public Boolean ServicesProcurementPayement { get; set; }
+        public Boolean ServicesProcurementManaging { get; set; }
 
+        //Acceptance Management
+        public Boolean ServicesAcceptanceInitiating { get; set; }
+        public Boolean ServicesAcceptanceDocumenting { get; set; }
+        public Boolean ServicesAcceptanceGainingFinalAcceptance { get; set; }
+        public Boolean ServicesAcceptanceKeeping { get; set; }
+
+        //Communications Management
+        public Boolean ServicesCommunicationsUndertaking { get; set; }
+        public Boolean ServicesCommunicationsCreating { get; set; }
+        public Boolean ServicesCommunicationsDistributing { get; set; }
+        public Boolean ServicesCommunicationsKeeping { get; set; }
+
+        //Stage Gate Reviews
+        public Boolean ServicesStageGateIdentifying { get; set; }
+        public Boolean ServicesStageGateOrganizing { get; set; }
+
+        //Auditing and Compliance
+        public Boolean ServicesAuditingEnsuringConforms { get; set; }
+        public Boolean ServicesAuditingInforming { get; set; }
+
+        //Supporting Staff
+        public Boolean ServicesSupportingAssisting { get; set; }
+        public Boolean ServicesSupportingAdvising{ get; set; }
+        public Boolean ServicesSupportingPaying { get; set; }
+
+        //Providing tools
+        public Boolean ServicesProvidingProjectManagement { get; set; }
+        public Boolean ServicesProvidingToolsForMonitoring { get; set; }
+        public Boolean ServicesProvidingTraining { get; set; }
+
+        //Performing administration  
+        public Boolean ServicesPerformingAdministration { get; set; }
+        public Boolean ServicesPerformingPurchasing { get; set; }
+
+        //Filing documents
+        public Boolean ServicesFilingLibrary { get; set; }
+        public Boolean ServicesFilingImplementing { get; set; }
+
+        //Undertaking closue reviews
+        public Boolean ServicesClosureOrganizing { get; set; }
+        public Boolean ServicesComminicating { get; set; }
     }
 }
