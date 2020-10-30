0123627e824c8ef91c506284bb9cb778a36a65cf
Gerard Jooste <28410688@student.g.nwu.ac.za>
2020-10-30 13:06:38 +0200

    Finished the ChangeRequest From as is

--- ProjectManagementToolkit/MPMM/MPMM Document Forms/ChangeRequestFormDocumentForm.cs
+++ ProjectManagementToolkit/MPMM/MPMM Document Forms/ChangeRequestFormDocumentForm.cs
@@ -13,6 +13,18 @@ namespace ProjectManagementToolkit.MPMM.MPMM_Document_Forms
     public partial class ChangeRequestFormDocumentForm : Form
     {
         private string projectName;
+        private string projectManager;
+        private string changeNumber;
+        private string changeRequester;
+        private DateTime changeRequestDate;
+        private string changeUrgancy;
+        private string changeDescription;
+        private string changeDrivers;
+        private string changeBenefits;
+        private string changeCost;
+        private string projectImpact;
+        private string supportingDocumentation;
+        private string signatue;
 
         public string ProjectName
         {
@@ -20,6 +32,76 @@ namespace ProjectManagementToolkit.MPMM.MPMM_Document_Forms
             set { projectName = value; }
         }
 
+        public string ProjectManager
+        {
+            get { return projectManager; }
+            set { projectManager = value; }
+        }
+
+        public string ChangeNumber
+        {
+            get { return changeNumber; }
+            set { changeNumber = value; }
+        }
+
+        public string ChangeRequester
+        {
+            get { return changeRequester; }
+            set { changeRequester = value; }
+        }
+        public DateTime ChangeRequestDate
+        {
+            get { return changeRequestDate; }
+            set { changeRequestDate = value; }
+        }
+
+        public string ChangeUrgancy
+        {
+            get { return changeUrgancy; }
+            set { changeUrgancy = value; }
+        }
+
+        public string ChangeDescription
+        {
+            get { return changeDescription; }
+            set { changeDescription = value; }
+        }
+
+        public string ChangeDrivers
+        {
+            get { return changeDrivers; }
+            set { changeDrivers = value; }
+        }
+
+        public string ChangeBenefits
+        {
+            get { return changeBenefits; }
+            set { changeBenefits = value; }
+        }
+
+        public string ChangeCost
+        {
+            get { return changeCost; }
+            set { changeCost = value; }
+        }
+
+        public string ProjectImpact
+        {
+            get { return projectImpact; }
+            set { projectImpact = value; }
+        }
+
+        public string SupportingDocumentation
+        {
+            get { return supportingDocumentation; }
+            set { supportingDocumentation = value; }
+        }
+
+        public string Signatue
+        {
+            get { return signatue; }
+            set { signatue = value; }
+        }
         public ChangeRequestFormDocumentForm()
         {
             InitializeComponent();
