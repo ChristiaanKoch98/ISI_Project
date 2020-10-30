bd2155cd8fb270ae3fe53952976300190f4dc668
Gerard Jooste <28410688@student.g.nwu.ac.za>
2020-10-30 16:39:46 +0200

    Finished will all of my forms/classes, including the Project Office Checklist one

---
+++ ProjectManagementToolkit/MPMM/MPMM Document Models/ProjectOfficeChecklistModel.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectManagementToolkit.MPMM.MPMM_Document_Models
+{
+    class ProjectOfficeChecklistModel
+    {
+        public string ProjectName { get; set; }
+
+        public string ProjectManager { get; set; }
+
+        public string ProjectOfficeManager { get; set; }
+
+        public Boolean PremisesProjectOfficeDocumented { get; set; }
+
+        public Boolean PremisesProjectOfficeProcured { get; set; }
+
+        public Boolean PremisesPracticalLocation { get; set; }
+
+        public Boolean PremisesRequiredDocumted { get; set; }
+
+        public Boolean PremisesFormalContract { get; set; }
+
+        public Boolean PremisesSufficientSpace { get; set; }
+
+        public Boolean PremisesContinueAvailible { get; set; }
+
+        public Boolean PremisesAdditionalFitOut { get; set; }
+
+        public Boolean PremisesOnSiteFacilities { get; set; }
+
+        public Boolean EquipmentRequiredOffice { get; set; }
+
+        public Boolean EquipmentMaintananceCOntracts { get; set; }
+
+        public Boolean EquipmentSpareEquipment { get; set; }
+
+        public Boolean EquipmentOfficeFunctioning { get; set; }
+
+        public Boolean EquipmentSufficientCommunication { get; set; }
+
+        public Boolean EquipmentVideoCOnferensing { get; set; }
+
+        public Boolean EquipmentFunctioningAsRequired { get; set; }
+
+        public Boolean RolesAppointedProjectSponsor { get; set; }
+        public Boolean RolesAppointedProjectManager { get; set; }
+        public Boolean RolesAppointedProjectOfficeManager { get; set; }
+        public Boolean RolesAppointedProcurementManager { get; set; }
+        public Boolean RolesAppointedCommunicationsManager { get; set; }
+        public Boolean RolesAppointedQualityManager { get; set; }
+        public Boolean RolesAppointedRiskManager { get; set; }
+        public Boolean RolesAppointedTeamLeader { get; set; }
+        public Boolean RolesJobDescriptionsDocumented { get; set; }
+        public Boolean RolesJobDescriptionsResponsibilities { get; set; }
+        public Boolean RolesSkilledPeopleAppointed { get; set; }
+
+        public Boolean StandardsIndustyStandards { get; set; }
+
+        public Boolean StandardsHealthAndSafety { get; set; }
+        public Boolean StandardsProjectPlanning { get; set; }
+        public Boolean StandardsPMI { get; set; }
+
+        public Boolean ProcessesTimeManagement { get; set; }
+        public Boolean ProcessesCostManagement { get; set; }
+        public Boolean ProcessesQualityManagement { get; set; }
+        public Boolean ProcessesChangeManagement { get; set; }
+        public Boolean ProcessesRiskManagement { get; set; }
+        public Boolean ProcessesIssueManagement { get; set; }
+        public Boolean ProcessesProcurementManagement { get; set; }
+        public Boolean ProcessesAcceptanceManagement { get; set; }
+        public Boolean ProcessesCommunicationsManagement { get; set; }
+
+        public Boolean TemplatesInitiationBusinessCase { get; set; }
+        public Boolean TemplatesInitiationFeasibilityStudy { get; set; }
+        public Boolean TemplatesInitiationTermsOfReference { get; set; }
+        public Boolean TemplatesInitiationJobDescription { get; set; }
+        public Boolean TemplatesInitiationStageGate { get; set; }
+
+        public Boolean TemplatesPlanningProjectPlan { get; set; }
+        public Boolean TemplatesPlanningResourcePlan { get; set; }
+        public Boolean TemplatesPlanningFinancialPlan { get; set; }
+        public Boolean TemplatesPlanningQualityPlan { get; set; }
+        public Boolean TemplatesPlanningRiskPlan { get; set; }
+
+        public Boolean TemplatesExecutionTimesheet { get; set; }
+        public Boolean TemplatesExecutionExpense { get; set; }
+        public Boolean TemplatesExecutionQuality { get; set; }
+        public Boolean TemplatesExecutionChange { get; set; }
+        public Boolean TemplatesExecutionRisk { get; set; }
+        public Boolean TemplatesExecutionIssue { get; set; }
+        public Boolean TemplatesExecutionPurchase { get; set; }
+        public Boolean TemplatesExecutionProcurement { get; set; }
+        public Boolean TemplatesExecutionProject { get; set; }
+        public Boolean TemplatesExecutionCommunication { get; set; }
+        public Boolean TemplatesExecutionAcceptance { get; set; }
+
+
+
+
+    }
+}
