59b4bf724981fe082258ee6acd00e5e13eeb14f0
Armand <armandbotha50@gmail.com>
2020-11-01 20:36:54 +0200

    Models added

---
+++ ProjectManagementToolkit/MPMM/MPMM Document Models/PhaseReviewFormExecutionModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectManagementToolkit.MPMM.MPMM_Document_Models
+{
+    class PhaseReviewFormExecutionModel
+    {
+        public string ProjectName { get; set; }
+        public string ProjectManager { get; set; }
+        public string ProjectSponsor { get; set; }
+        public string ReportPreparedBy { get; set; }
+        public DateTime ReportPreparationDate { get; set; }
+
+        public string Summary { get; set; }
+        public string ProjectSchedule { get; set; }
+        public string ProjectExpenses { get; set; }
+        public string ProjectDeliverables { get; set; }
+        public string ProjectRisks { get; set; }
+        public string ProjectIssues { get; set; }
+        public string ProjectChanges { get; set; }
+
+        public List<ReviewDetial> ReviewDetials { get; set; }
+        public string SupportingDocumentation { get; set; }
+        public string Signature { get; set; }
+        public DateTime SignatureDate { get; set; } 
+
+        public class ReportingPeriod 
+        {
+            public DateTime StartingDate { get; set; }
+            public DateTime EndDate { get; set; }
+        }
+
+        public class ReviewDetial
+        {
+            public string ReviewCategory { get; set; }
+            public string ReviewQuestion { get; set; }
+            public bool Answer { get; set; }
+            public string Varaince { get; set; }
+        }
+    }
+}
---
+++ ProjectManagementToolkit/MPMM/MPMM Document Models/ProjectStatusReportModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectManagementToolkit.MPMM.MPMM_Document_Models
+{
+    class ProjectStatusReportModel
+    {
+        public string ProjectName { get; set; }
+        public string ProjectID { get; set; }
+        public string ProjectManager { get; set; }
+        public string ProjectSponsor { get; set; }
+        public string ReportPreparedBy { get; set; }
+        public DateTime ReportPreparationDate { get; set; }
+        public string Recipients { get; set; }
+
+        public string ProjectDescription { get; set; }
+        public string OverallStatus { get; set; }
+        public string ProjectSchedule { get; set; }
+        public string ProjectExpenses { get; set; }
+        public string ProjectDeliverables { get; set; }
+        public string ProjectRisks { get; set; }
+        public string ProjectIssues { get; set; }
+        public string ProjectChanges { get; set; }
+
+        public string DSSchedule { get; set; }
+        public string DSExpenses { get; set; }
+        public string DSEffort { get; set; }
+        public string DSRisks { get; set; }
+        public string DSIssues { get; set; }
+        public string DSQuality { get; set; }
+
+        public List<ReviewDetial> ReviewDetials { get; set; }
+        public string SupportingDocumentation { get; set; }
+        public string Signature { get; set; }
+        public DateTime SignatureDate { get; set; } 
+
+        public class ReportingPeriod 
+        {
+            public DateTime StartingDate { get; set; }
+            public DateTime EndDate { get; set; }
+        }
+
+        public class ReviewDetial
+        {
+            public string ReviewCategory { get; set; }
+            public string ReviewQuestion { get; set; }
+            public bool Answer { get; set; }
+            public string Varaince { get; set; }
+        }
+    }
+}
