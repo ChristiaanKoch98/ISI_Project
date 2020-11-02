e9705dd35d160caa2b74c3b2717007f422aad73a
Fraser <28635248@student.g.nwu.ac.za>
2020-11-02 19:22:19 +0200

    Project Selection Page Base
    A base version of a project selection page has been made, further changes probably needed

---
+++ ProjectManagementToolkit/MPMM/MPMM Document Forms/ProjectSelection.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace ProjectManagementToolkit.MPMM.MPMM_Document_Forms
+{
+    partial class ProjectSelection
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtProjectName = new System.Windows.Forms.TextBox();
+            this.btnNewProject = new System.Windows.Forms.Button();
+            this.comboBoxProjectType = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBoxExistingProjects = new System.Windows.Forms.ListBox();
+            this.btnProjectSelection = new System.Windows.Forms.Button();
+            this.btnStartProject = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.btnStartProject);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.comboBoxProjectType);
+            this.groupBox1.Controls.Add(this.txtProjectName);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.ForeColor = System.Drawing.Color.White;
+            this.groupBox1.Location = new System.Drawing.Point(12, 47);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(182, 212);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "New Project";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Project Name";
+            // 
+            // txtProjectName
+            // 
+            this.txtProjectName.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtProjectName.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(36)))), ((int)(((byte)(49)))));
+            this.txtProjectName.ForeColor = System.Drawing.Color.White;
+            this.txtProjectName.Location = new System.Drawing.Point(76, 17);
+            this.txtProjectName.Name = "txtProjectName";
+            this.txtProjectName.Size = new System.Drawing.Size(100, 20);
+            this.txtProjectName.TabIndex = 1;
+            // 
+            // btnNewProject
+            // 
+            this.btnNewProject.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnNewProject.BackColor = System.Drawing.Color.White;
+            this.btnNewProject.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(36)))), ((int)(((byte)(49)))));
+            this.btnNewProject.Location = new System.Drawing.Point(12, 13);
+            this.btnNewProject.Name = "btnNewProject";
+            this.btnNewProject.Size = new System.Drawing.Size(182, 28);
+            this.btnNewProject.TabIndex = 1;
+            this.btnNewProject.Text = "New Project";
+            this.btnNewProject.UseVisualStyleBackColor = false;
+            // 
+            // comboBoxProjectType
+            // 
+            this.comboBoxProjectType.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBoxProjectType.FormattingEnabled = true;
+            this.comboBoxProjectType.Items.AddRange(new object[] {
+            "MPMM",
+            "Prince2"});
+            this.comboBoxProjectType.Location = new System.Drawing.Point(76, 43);
+            this.comboBoxProjectType.Name = "comboBoxProjectType";
+            this.comboBoxProjectType.Size = new System.Drawing.Size(100, 20);
+            this.comboBoxProjectType.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Project Type";
+            // 
+            // listBoxExistingProjects
+            // 
+            this.listBoxExistingProjects.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBoxExistingProjects.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(36)))), ((int)(((byte)(49)))));
+            this.listBoxExistingProjects.ForeColor = System.Drawing.Color.White;
+            this.listBoxExistingProjects.FormattingEnabled = true;
+            this.listBoxExistingProjects.ItemHeight = 12;
+            this.listBoxExistingProjects.Location = new System.Drawing.Point(201, 13);
+            this.listBoxExistingProjects.Name = "listBoxExistingProjects";
+            this.listBoxExistingProjects.Size = new System.Drawing.Size(316, 208);
+            this.listBoxExistingProjects.TabIndex = 2;
+            // 
+            // btnProjectSelection
+            // 
+            this.btnProjectSelection.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnProjectSelection.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(36)))), ((int)(((byte)(49)))));
+            this.btnProjectSelection.Location = new System.Drawing.Point(258, 229);
+            this.btnProjectSelection.Name = "btnProjectSelection";
+            this.btnProjectSelection.Size = new System.Drawing.Size(202, 30);
+            this.btnProjectSelection.TabIndex = 3;
+            this.btnProjectSelection.Text = "Select Project";
+            this.btnProjectSelection.UseVisualStyleBackColor = true;
+            // 
+            // btnStartProject
+            // 
+            this.btnStartProject.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnStartProject.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(36)))), ((int)(((byte)(49)))));
+            this.btnStartProject.Location = new System.Drawing.Point(52, 183);
+            this.btnStartProject.Name = "btnStartProject";
+            this.btnStartProject.Size = new System.Drawing.Size(75, 23);
+            this.btnStartProject.TabIndex = 4;
+            this.btnStartProject.Text = "Start Project";
+            this.btnStartProject.UseVisualStyleBackColor = true;
+            // 
+            // ProjectSelection
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(36)))), ((int)(((byte)(49)))));
+            this.ClientSize = new System.Drawing.Size(529, 271);
+            this.Controls.Add(this.btnProjectSelection);
+            this.Controls.Add(this.listBoxExistingProjects);
+            this.Controls.Add(this.btnNewProject);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Cambria", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.White;
+            this.Name = "ProjectSelection";
+            this.Text = "ProjectSelection";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnStartProject;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBoxProjectType;
+        private System.Windows.Forms.TextBox txtProjectName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnNewProject;
+        private System.Windows.Forms.ListBox listBoxExistingProjects;
+        private System.Windows.Forms.Button btnProjectSelection;
+    }
+}
