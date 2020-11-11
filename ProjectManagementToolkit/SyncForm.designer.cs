ca5b73064cf194268ad9c21a13198bd402f83820
Hannes <rooshannes@live.com>
2020-11-11 17:01:58 +0200

    Project File Update

---
+++ ProjectManagementToolkit/SyncForm.designer.cs
@@ -0,0 +1,87 @@
+﻿namespace ProjectManagementToolkit.MPMM
+{
+    partial class SyncForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.syncProgressBar = new System.Windows.Forms.ProgressBar();
+            this.lblProgress = new System.Windows.Forms.Label();
+            this.btnSync = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // syncProgressBar
+            // 
+            this.syncProgressBar.Location = new System.Drawing.Point(12, 21);
+            this.syncProgressBar.Name = "syncProgressBar";
+            this.syncProgressBar.Size = new System.Drawing.Size(969, 49);
+            this.syncProgressBar.TabIndex = 0;
+            // 
+            // lblProgress
+            // 
+            this.lblProgress.AutoSize = true;
+            this.lblProgress.ForeColor = System.Drawing.Color.White;
+            this.lblProgress.Location = new System.Drawing.Point(12, 85);
+            this.lblProgress.Name = "lblProgress";
+            this.lblProgress.Size = new System.Drawing.Size(73, 17);
+            this.lblProgress.TabIndex = 1;
+            this.lblProgress.Text = "Progress: ";
+            // 
+            // btnSync
+            // 
+            this.btnSync.Location = new System.Drawing.Point(15, 115);
+            this.btnSync.Name = "btnSync";
+            this.btnSync.Size = new System.Drawing.Size(113, 37);
+            this.btnSync.TabIndex = 2;
+            this.btnSync.Text = "Sync Now";
+            this.btnSync.UseVisualStyleBackColor = true;
+            this.btnSync.Click += new System.EventHandler(this.btnSync_Click);
+            // 
+            // SyncForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(36)))), ((int)(((byte)(49)))));
+            this.ClientSize = new System.Drawing.Size(1014, 172);
+            this.Controls.Add(this.btnSync);
+            this.Controls.Add(this.lblProgress);
+            this.Controls.Add(this.syncProgressBar);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "SyncForm";
+            this.Text = "Sync Form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar syncProgressBar;
+        private System.Windows.Forms.Label lblProgress;
+        private System.Windows.Forms.Button btnSync;
+    }
+}
