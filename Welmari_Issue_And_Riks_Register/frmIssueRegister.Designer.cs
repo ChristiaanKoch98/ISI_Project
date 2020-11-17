09df139c9b569b00b087a65f0dc7b97ade5aa9f1
Welmari Odendaal <71969087+Welmari@users.noreply.github.com>
2020-11-17 03:50:19 +0200

    Add files via upload

---
+++ Welmari_Issue_And_Riks_Register/frmIssueRegister.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace Welmari_Issue_And_Riks_Register
+{
+    partial class frmIssueRegister
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label27 = new System.Windows.Forms.Label();
+            this.txtIssueRegisterProjectName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtIssueRegisterProjectManager = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.dataGridViewSolutionRaiseRaised = new System.Windows.Forms.DataGridView();
+            this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Date_Raised = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Raised_By = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Received_By = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Description_of_Issue = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Impact = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Priority = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Action = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Owner = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Outcome = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Date_for_Resolution = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Date_Resolved = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewSolutionRaiseRaised)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label27
+            // 
+            this.label27.AutoSize = true;
+            this.label27.Font = new System.Drawing.Font("Cambria", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label27.ForeColor = System.Drawing.Color.White;
+            this.label27.Location = new System.Drawing.Point(14, 14);
+            this.label27.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label27.Name = "label27";
+            this.label27.Size = new System.Drawing.Size(70, 12);
+            this.label27.TabIndex = 18;
+            this.label27.Text = "Project Name:";
+            // 
+            // txtIssueRegisterProjectName
+            // 
+            this.txtIssueRegisterProjectName.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(36)))), ((int)(((byte)(49)))));
+            this.txtIssueRegisterProjectName.Font = new System.Drawing.Font("Cambria", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtIssueRegisterProjectName.ForeColor = System.Drawing.Color.White;
+            this.txtIssueRegisterProjectName.Location = new System.Drawing.Point(107, 11);
+            this.txtIssueRegisterProjectName.Margin = new System.Windows.Forms.Padding(4);
+            this.txtIssueRegisterProjectName.Name = "txtIssueRegisterProjectName";
+            this.txtIssueRegisterProjectName.Size = new System.Drawing.Size(116, 20);
+            this.txtIssueRegisterProjectName.TabIndex = 17;
+            this.txtIssueRegisterProjectName.Text = "Project Name";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Cambria", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(14, 42);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 12);
+            this.label1.TabIndex = 20;
+            this.label1.Text = "Project Manager:";
+            // 
+            // txtIssueRegisterProjectManager
+            // 
+            this.txtIssueRegisterProjectManager.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(36)))), ((int)(((byte)(49)))));
+            this.txtIssueRegisterProjectManager.Font = new System.Drawing.Font("Cambria", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtIssueRegisterProjectManager.ForeColor = System.Drawing.Color.White;
+            this.txtIssueRegisterProjectManager.Location = new System.Drawing.Point(107, 39);
+            this.txtIssueRegisterProjectManager.Margin = new System.Windows.Forms.Padding(4);
+            this.txtIssueRegisterProjectManager.Name = "txtIssueRegisterProjectManager";
+            this.txtIssueRegisterProjectManager.Size = new System.Drawing.Size(116, 20);
+            this.txtIssueRegisterProjectManager.TabIndex = 19;
+            this.txtIssueRegisterProjectManager.Text = "Project Manager";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.LightGray;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.dataGridViewSolutionRaiseRaised);
+            this.panel1.Location = new System.Drawing.Point(18, 72);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1098, 509);
+            this.panel1.TabIndex = 21;
+            // 
+            // dataGridViewSolutionRaiseRaised
+            // 
+            this.dataGridViewSolutionRaiseRaised.AllowUserToOrderColumns = true;
+            this.dataGridViewSolutionRaiseRaised.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewSolutionRaiseRaised.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ID,
+            this.Date_Raised,
+            this.Raised_By,
+            this.Received_By,
+            this.Description_of_Issue,
+            this.Impact,
+            this.Priority,
+            this.Action,
+            this.Owner,
+            this.Outcome,
+            this.Date_for_Resolution,
+            this.Date_Resolved});
+            this.dataGridViewSolutionRaiseRaised.Location = new System.Drawing.Point(3, 3);
+            this.dataGridViewSolutionRaiseRaised.Name = "dataGridViewSolutionRaiseRaised";
+            this.dataGridViewSolutionRaiseRaised.RowHeadersWidth = 51;
+            this.dataGridViewSolutionRaiseRaised.Size = new System.Drawing.Size(1088, 499);
+            this.dataGridViewSolutionRaiseRaised.TabIndex = 22;
+            // 
+            // ID
+            // 
+            this.ID.HeaderText = "ID";
+            this.ID.Name = "ID";
+            // 
+            // Date_Raised
+            // 
+            this.Date_Raised.HeaderText = "Date_Raised";
+            this.Date_Raised.Name = "Date_Raised";
+            // 
+            // Raised_By
+            // 
+            this.Raised_By.HeaderText = "Raised_By";
+            this.Raised_By.Name = "Raised_By";
+            // 
+            // Received_By
+            // 
+            this.Received_By.HeaderText = "Received_By";
+            this.Received_By.Name = "Received_By";
+            // 
+            // Description_of_Issue
+            // 
+            this.Description_of_Issue.HeaderText = "Description_of_Issue";
+            this.Description_of_Issue.Name = "Description_of_Issue";
+            // 
+            // Impact
+            // 
+            this.Impact.HeaderText = "Impact";
+            this.Impact.Name = "Impact";
+            // 
+            // Priority
+            // 
+            this.Priority.HeaderText = "Priority";
+            this.Priority.Name = "Priority";
+            // 
+            // Action
+            // 
+            this.Action.HeaderText = "Action";
+            this.Action.Name = "Action";
+            // 
+            // Owner
+            // 
+            this.Owner.HeaderText = "Owner";
+            this.Owner.Name = "Owner";
+            // 
+            // Outcome
+            // 
+            this.Outcome.HeaderText = "Outcome";
+            this.Outcome.Name = "Outcome";
+            // 
+            // Date_for_Resolution
+            // 
+            this.Date_for_Resolution.HeaderText = "Date_for_Resolution";
+            this.Date_for_Resolution.Name = "Date_for_Resolution";
+            // 
+            // Date_Resolved
+            // 
+            this.Date_Resolved.HeaderText = "Date_Resolved";
+            this.Date_Resolved.Name = "Date_Resolved";
+            // 
+            // frmIssueRegister
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(36)))), ((int)(((byte)(49)))));
+            this.ClientSize = new System.Drawing.Size(1135, 598);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtIssueRegisterProjectManager);
+            this.Controls.Add(this.label27);
+            this.Controls.Add(this.txtIssueRegisterProjectName);
+            this.Font = new System.Drawing.Font("Cambria", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "frmIssueRegister";
+            this.Text = "Issue Register";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewSolutionRaiseRaised)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label27;
+        private System.Windows.Forms.TextBox txtIssueRegisterProjectName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtIssueRegisterProjectManager;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataGridView dataGridViewSolutionRaiseRaised;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Date_Raised;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Raised_By;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Received_By;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Description_of_Issue;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Impact;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Priority;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Action;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Owner;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Outcome;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Date_for_Resolution;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Date_Resolved;
+    }
+}
+
---
+++ Welmari_Issue_And_Riks_Register/frmIssueRegister.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Welmari_Issue_And_Riks_Register
+{
+    public partial class frmIssueRegister : Form
+    {
+        public frmIssueRegister()
+        {
+            InitializeComponent();
+        }
+    }
+}
