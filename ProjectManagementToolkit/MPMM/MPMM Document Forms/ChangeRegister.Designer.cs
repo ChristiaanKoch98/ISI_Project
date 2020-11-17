0a1d51b9df371df594ee26872eb323754dae2650
Duan van Heerden <duanvheerden@gmail.com>
2020-11-17 22:11:00 +0200

    Change Register
    Created Change Register Form and all of its components.

---
+++ ProjectManagementToolkit/MPMM/MPMM Document Forms/ChangeRegister.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace ProjectManagementToolkit.MPMM.MPMM_Document_Forms
+{
+    partial class ChangeRegister
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Conformance_of_Processes_dgv = new System.Windows.Forms.DataGridView();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Project_Manager_tbx = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Enter_btn = new System.Windows.Forms.Button();
+            this.Project_Name_tbx = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.Conformance_of_Processes_dgv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Conformance_of_Processes_dgv
+            // 
+            this.Conformance_of_Processes_dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Conformance_of_Processes_dgv.Location = new System.Drawing.Point(18, 186);
+            this.Conformance_of_Processes_dgv.Name = "Conformance_of_Processes_dgv";
+            this.Conformance_of_Processes_dgv.Size = new System.Drawing.Size(1088, 150);
+            this.Conformance_of_Processes_dgv.TabIndex = 27;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Cambria", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(11, 23);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(259, 41);
+            this.label4.TabIndex = 25;
+            this.label4.Text = "Change Register";
+            // 
+            // Project_Manager_tbx
+            // 
+            this.Project_Manager_tbx.Location = new System.Drawing.Point(150, 123);
+            this.Project_Manager_tbx.Name = "Project_Manager_tbx";
+            this.Project_Manager_tbx.Size = new System.Drawing.Size(237, 20);
+            this.Project_Manager_tbx.TabIndex = 22;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(14, 124);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(127, 19);
+            this.label2.TabIndex = 21;
+            this.label2.Text = "Project Manager:";
+            // 
+            // Enter_btn
+            // 
+            this.Enter_btn.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.Enter_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Enter_btn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(36)))), ((int)(((byte)(49)))));
+            this.Enter_btn.Location = new System.Drawing.Point(509, 82);
+            this.Enter_btn.Name = "Enter_btn";
+            this.Enter_btn.Size = new System.Drawing.Size(75, 23);
+            this.Enter_btn.TabIndex = 20;
+            this.Enter_btn.Text = "Enter";
+            this.Enter_btn.UseVisualStyleBackColor = false;
+            // 
+            // Project_Name_tbx
+            // 
+            this.Project_Name_tbx.Location = new System.Drawing.Point(150, 85);
+            this.Project_Name_tbx.Name = "Project_Name_tbx";
+            this.Project_Name_tbx.Size = new System.Drawing.Size(237, 20);
+            this.Project_Name_tbx.TabIndex = 19;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(35, 86);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 19);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "Project Name:";
+            // 
+            // ChangeRegister
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(36)))), ((int)(((byte)(49)))));
+            this.ClientSize = new System.Drawing.Size(1215, 469);
+            this.Controls.Add(this.Conformance_of_Processes_dgv);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.Project_Manager_tbx);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Enter_btn);
+            this.Controls.Add(this.Project_Name_tbx);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Cambria", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.Name = "ChangeRegister";
+            this.Text = "ChangeRegister";
+            ((System.ComponentModel.ISupportInitialize)(this.Conformance_of_Processes_dgv)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView Conformance_of_Processes_dgv;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox Project_Manager_tbx;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button Enter_btn;
+        private System.Windows.Forms.TextBox Project_Name_tbx;
+        private System.Windows.Forms.Label label1;
+    }
+}
