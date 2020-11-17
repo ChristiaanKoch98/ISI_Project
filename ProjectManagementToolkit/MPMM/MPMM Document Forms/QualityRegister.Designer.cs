c164d356ee8221f88864a23ffdc81babeb6b325d
Duan van Heerden <duanvheerden@gmail.com>
2020-11-17 22:03:33 +0200

    Quality Register
    Created Quality Register Form and all of its components.

---
+++ ProjectManagementToolkit/MPMM/MPMM Document Forms/QualityRegister.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace ProjectManagementToolkit.MPMM.MPMM_Document_Forms
+{
+    partial class QualityRegister
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Enter_btn = new System.Windows.Forms.Button();
+            this.Project_Name_tbx = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Project_Manager_tbx = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Quality_Manager_tbx = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.Conformance_of_Processes_dgv = new System.Windows.Forms.DataGridView();
+            this.Quality_of_Deliverables_dgv = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.Conformance_of_Processes_dgv)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Quality_of_Deliverables_dgv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Enter_btn
+            // 
+            this.Enter_btn.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.Enter_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Enter_btn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(36)))), ((int)(((byte)(49)))));
+            this.Enter_btn.Location = new System.Drawing.Point(510, 68);
+            this.Enter_btn.Name = "Enter_btn";
+            this.Enter_btn.Size = new System.Drawing.Size(75, 23);
+            this.Enter_btn.TabIndex = 9;
+            this.Enter_btn.Text = "Enter";
+            this.Enter_btn.UseVisualStyleBackColor = false;
+            // 
+            // Project_Name_tbx
+            // 
+            this.Project_Name_tbx.Location = new System.Drawing.Point(151, 71);
+            this.Project_Name_tbx.Name = "Project_Name_tbx";
+            this.Project_Name_tbx.Size = new System.Drawing.Size(237, 20);
+            this.Project_Name_tbx.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(36, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 19);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Project Name:";
+            // 
+            // Project_Manager_tbx
+            // 
+            this.Project_Manager_tbx.Location = new System.Drawing.Point(151, 109);
+            this.Project_Manager_tbx.Name = "Project_Manager_tbx";
+            this.Project_Manager_tbx.Size = new System.Drawing.Size(237, 20);
+            this.Project_Manager_tbx.TabIndex = 11;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(15, 110);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(127, 19);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Project Manager:";
+            // 
+            // Quality_Manager_tbx
+            // 
+            this.Quality_Manager_tbx.Location = new System.Drawing.Point(151, 150);
+            this.Quality_Manager_tbx.Name = "Quality_Manager_tbx";
+            this.Quality_Manager_tbx.Size = new System.Drawing.Size(237, 20);
+            this.Quality_Manager_tbx.TabIndex = 13;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(15, 151);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(126, 19);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Quality Manager:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Cambria", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(33, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(258, 41);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Quality Register";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Cambria", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(34, 204);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(315, 32);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Conformance of Processes";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Cambria", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(34, 417);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(275, 32);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Quality of Deliverables";
+            // 
+            // Conformance_of_Processes_dgv
+            // 
+            this.Conformance_of_Processes_dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Conformance_of_Processes_dgv.Location = new System.Drawing.Point(40, 249);
+            this.Conformance_of_Processes_dgv.Name = "Conformance_of_Processes_dgv";
+            this.Conformance_of_Processes_dgv.Size = new System.Drawing.Size(1088, 150);
+            this.Conformance_of_Processes_dgv.TabIndex = 17;
+            // 
+            // Quality_of_Deliverables_dgv
+            // 
+            this.Quality_of_Deliverables_dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Quality_of_Deliverables_dgv.Location = new System.Drawing.Point(40, 463);
+            this.Quality_of_Deliverables_dgv.Name = "Quality_of_Deliverables_dgv";
+            this.Quality_of_Deliverables_dgv.Size = new System.Drawing.Size(1088, 147);
+            this.Quality_of_Deliverables_dgv.TabIndex = 18;
+            // 
+            // QualityRegister
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(36)))), ((int)(((byte)(49)))));
+            this.ClientSize = new System.Drawing.Size(1181, 674);
+            this.Controls.Add(this.Quality_of_Deliverables_dgv);
+            this.Controls.Add(this.Conformance_of_Processes_dgv);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.Quality_Manager_tbx);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Project_Manager_tbx);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Enter_btn);
+            this.Controls.Add(this.Project_Name_tbx);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Cambria", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.Name = "QualityRegister";
+            this.Text = "QualityRegister";
+            ((System.ComponentModel.ISupportInitialize)(this.Conformance_of_Processes_dgv)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Quality_of_Deliverables_dgv)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Enter_btn;
+        private System.Windows.Forms.TextBox Project_Name_tbx;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Project_Manager_tbx;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Quality_Manager_tbx;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DataGridView Conformance_of_Processes_dgv;
+        private System.Windows.Forms.DataGridView Quality_of_Deliverables_dgv;
+    }
+}
