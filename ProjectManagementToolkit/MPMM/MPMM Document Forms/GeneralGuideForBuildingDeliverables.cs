cc782ad41f477d73d58700c8c01f88f703c75cfb
Orifha1 <orifhatshikotshi1@gmail.com>
2020-11-17 16:35:48 +0200

    Add General_Guide_For_Building_Deliverables_Form

---
+++ ProjectManagementToolkit/MPMM/MPMM Document Forms/GeneralGuideForBuildingDeliverables.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjectManagementToolkit.MPMM.MPMM_Document_Forms
+{
+    public partial class GeneralGuideForBuildingDeliverables : Form
+    {
+        public GeneralGuideForBuildingDeliverables()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox11_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
