508433a12dd971463c296089f9cbb44591c9b5a5
Fraser <28635248@student.g.nwu.ac.za>
2020-11-29 20:02:51 +0200

    Register forms changes
    Changes added to register forms including colours and other fixes

--- ProjectManagementToolkit/MPMM/MPMM Document Forms/ExpenseRegister.Designer.cs
+++ ProjectManagementToolkit/MPMM/MPMM Document Forms/ExpenseRegister.Designer.cs
@@ -43,9 +43,9 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Location = new System.Drawing.Point(12, 17);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(70, 12);
+            this.label1.Size = new System.Drawing.Size(76, 14);
             this.label1.TabIndex = 0;
             this.label1.Text = "Project Name:";
             // 
@@ -55,9 +55,9 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 54);
+            this.label2.Location = new System.Drawing.Point(12, 63);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(86, 12);
+            this.label2.Size = new System.Drawing.Size(94, 14);
             this.label2.TabIndex = 1;
             this.label2.Text = "Project Manager: ";
             // 
@@ -66,11 +66,11 @@
             this.txtProjectName.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.txtProjectName.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(36)))), ((int)(((byte)(49)))));
-            this.txtProjectName.ForeColor = System.Drawing.Color.White;
-            this.txtProjectName.Location = new System.Drawing.Point(104, 12);
+            this.txtProjectName.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(209)))), ((int)(((byte)(237)))), ((int)(((byte)(242)))));
+            this.txtProjectName.ForeColor = System.Drawing.Color.Black;
+            this.txtProjectName.Location = new System.Drawing.Point(104, 14);
             this.txtProjectName.Name = "txtProjectName";
-            this.txtProjectName.Size = new System.Drawing.Size(100, 20);
+            this.txtProjectName.Size = new System.Drawing.Size(100, 21);
             this.txtProjectName.TabIndex = 2;
             this.txtProjectName.Text = "Project Name";
             // 
@@ -79,11 +79,11 @@
             this.txtProjectManager.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.txtProjectManager.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(36)))), ((int)(((byte)(49)))));
-            this.txtProjectManager.ForeColor = System.Drawing.Color.White;
-            this.txtProjectManager.Location = new System.Drawing.Point(104, 51);
+            this.txtProjectManager.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(209)))), ((int)(((byte)(237)))), ((int)(((byte)(242)))));
+            this.txtProjectManager.ForeColor = System.Drawing.Color.Black;
+            this.txtProjectManager.Location = new System.Drawing.Point(104, 59);
             this.txtProjectManager.Name = "txtProjectManager";
-            this.txtProjectManager.Size = new System.Drawing.Size(100, 20);
+            this.txtProjectManager.Size = new System.Drawing.Size(100, 21);
             this.txtProjectManager.TabIndex = 3;
             this.txtProjectManager.Text = "Project Manager";
             // 
@@ -93,36 +93,37 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.dataGridViewExpenseRegister.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridViewExpenseRegister.Location = new System.Drawing.Point(12, 86);
+            this.dataGridViewExpenseRegister.Location = new System.Drawing.Point(12, 100);
             this.dataGridViewExpenseRegister.Name = "dataGridViewExpenseRegister";
-            this.dataGridViewExpenseRegister.Size = new System.Drawing.Size(775, 317);
+            this.dataGridViewExpenseRegister.Size = new System.Drawing.Size(775, 370);
             this.dataGridViewExpenseRegister.TabIndex = 4;
             // 
             // btnEnterData
             // 
+            this.btnEnterData.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(73)))), ((int)(((byte)(173)))), ((int)(((byte)(252)))));
             this.btnEnterData.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(36)))), ((int)(((byte)(49)))));
-            this.btnEnterData.Location = new System.Drawing.Point(712, 34);
+            this.btnEnterData.Location = new System.Drawing.Point(712, 40);
             this.btnEnterData.Name = "btnEnterData";
-            this.btnEnterData.Size = new System.Drawing.Size(75, 23);
+            this.btnEnterData.Size = new System.Drawing.Size(75, 27);
             this.btnEnterData.TabIndex = 5;
             this.btnEnterData.Text = "Enter Data";
-            this.btnEnterData.UseVisualStyleBackColor = true;
+            this.btnEnterData.UseVisualStyleBackColor = false;
             this.btnEnterData.Click += new System.EventHandler(this.btnEnterData_Click);
             // 
             // ExpenseRegister
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 14F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(36)))), ((int)(((byte)(49)))));
-            this.ClientSize = new System.Drawing.Size(800, 415);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(209)))), ((int)(((byte)(237)))), ((int)(((byte)(242)))));
+            this.ClientSize = new System.Drawing.Size(800, 484);
             this.Controls.Add(this.btnEnterData);
             this.Controls.Add(this.dataGridViewExpenseRegister);
             this.Controls.Add(this.txtProjectManager);
             this.Controls.Add(this.txtProjectName);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Font = new System.Drawing.Font("Cambria", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ForeColor = System.Drawing.Color.White;
+            this.Font = new System.Drawing.Font("Helvetica Light", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.Black;
             this.Name = "ExpenseRegister";
             this.Text = "ExpenseRegister";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewExpenseRegister)).EndInit();
